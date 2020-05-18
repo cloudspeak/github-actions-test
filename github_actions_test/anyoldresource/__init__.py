9f4c8cc8726e32b333965c363cb20a7339cf123c
Rob Frampton <cloudspeaksoftware@gmail.com>
2020-05-18 16:37:22 +0000

    Testcommit

---
+++ github_actions_test/anyoldresource/__init__.py
@@ -0,0 +1,4 @@
+from .anyoldresource import Anyoldresource
+from .anyoldresource_provider import AnyoldresourceProvider
+
+__all__ = ["Anyoldresource", "AnyoldresourceProvider"]
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="github_actions_test",
+    version="0.0.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="fdkfjdkf",
+    packages=find_packages(exclude=("example")),
+    python_requires=">=3.7",
+    install_requires=["pulumi>=1.8.1",],
+    test_requires=["pulumi>=1.8.1",],
+    test_suite="test",
+)
